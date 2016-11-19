b2586fa222758c3b2fc848b96de66d20c1630a18
Ricky Pinder <pinderrr@outlook.com>
2016-11-19 21:42:12 +0000

    zsh and fonts install is now above dotfile systemlink

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+sudo apt-get install zsh
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s $(which zsh)
+rm ~/.zshrc
+cd
+git clone https://github.com/powerline/fonts
+cd fonts
+./install.sh
+
 for f in .*
 do
     if [ "$f" = ".git" ]
@@ -8,15 +18,5 @@ do
     echo "$(pwd)/$f"
     ln -s $(pwd)/$f ~/$f
 done
-
-sudo apt-get install zsh
-sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-echo "DEFAULT_USER is: "
-read DEFAULT_USER
-echo $DEFAULT_USER > .zshrc
-cd
-git clone https://github.com/powerline/fonts
-cd fonts
-./install.sh
 
 xrdb -load ~/.Xresources
