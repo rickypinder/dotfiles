a2b1e2a1a02f3f3104eb7dceea1ee4e784af487d
Ricky Pinder <pinderrr@outlook.com>
2016-12-22 20:37:48 +0000

    moved customization options to custom.el

--- emacs/init.el
+++ emacs/init.el
@@ -21,15 +21,19 @@
 ;;   - fuzzy finder (?)
 ;; + make my .emacs more organised
 ;;   - documentation
-;;     - file size doesn't matter, lots of comments 
 ;;   - re-order parts
-;;   - use multiple files
-;;   - move to init.el
 ;;   - use-package (?)
 ;;   - org mode integration (?)
 ;; + smooth scrolling
+;; + email inside emacs
+;; + set up irc
+;;   - facebook messanger bitbee thing
+;; + latex 
 
 ;-------------------------------------------------------------------------------
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -163,7 +167,6 @@ Return a list of installed packages or nil for every skipped package."
                           'key-chord
                           'winum
                           'which-key
-                          'git-gutter
                           'github-browse-file
                           )
 
@@ -277,57 +280,6 @@ Return a list of installed packages or nil for every skipped package."
 
 (require 'which-key)
 (which-key-mode)
-
-(require 'git-gutter)
-(global-git-gutter-mode t)
-(git-gutter:linum-setup)
-(global-set-key (kbd "C-c C-k") 'git-gutter:previous-hunk)
-(global-set-key (kbd "C-c C-j") 'git-gutter:next-hunk)
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Meslo LG M DZ for Powerline" :foundry "PfEd" :slant normal :weight normal :height 113 :width normal))))
- '(linum-highlight-face ((t (:background "#343d46" :foreground "yellow")))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(custom-safe-themes
-   (quote
-    ("d8f76414f8f2dcb045a37eb155bfaa2e1d17b6573ed43fb1d18b936febc7bbc2" "1b27e3b3fce73b72725f3f7f040fd03081b576b1ce8bbdfcb0212920aec190ad" "64ca5a1381fa96cb86fd6c6b4d75b66dc9c4e0fc1288ee7d914ab8d2638e23a9" "721bb3cb432bb6be7c58be27d583814e9c56806c06b4077797074b009f322509" "946e871c780b159c4bb9f580537e5d2f7dba1411143194447604ecbaf01bd90c" default)))
- '(fci-rule-color "#343d46")
- '(git-gutter:update-interval 1)
- '(package-selected-packages
-   (quote
-    (github-browse-file git-gutter which-key winum key-chord pdf-tools hlinum switch-window smex rbenv quickrun org-bullets neotree magit ido-vertical-mode ido-ubiquitous flycheck evil-surround base16-theme)))
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#bf616a")
-     (40 . "#DCA432")
-     (60 . "#ebcb8b")
-     (80 . "#B4EB89")
-     (100 . "#89EBCA")
-     (120 . "#89AAEB")
-     (140 . "#C189EB")
-     (160 . "#bf616a")
-     (180 . "#DCA432")
-     (200 . "#ebcb8b")
-     (220 . "#B4EB89")
-     (240 . "#89EBCA")
-     (260 . "#89AAEB")
-     (280 . "#C189EB")
-     (300 . "#bf616a")
-     (320 . "#DCA432")
-     (340 . "#ebcb8b")
-     (360 . "#B4EB89"))))
- '(vc-annotate-very-old-color nil))
 
 ;; This makes the inactive modeline darker than the active modeline. This
 ;; probably isn't the best solution but this apparently needs to be ran after the
