883c84015f0fa11bc60b816adcef5cac90240b10
Ricky Pinder <pinderrr@outlook.com>
2017-08-05 22:06:08 +0100

    elpa packages from repo

--- emacs/.emacs.d/elpa/github-browse-file-20160205.627/github-browse-file-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "github-browse-file" "20160205.627" "View the file you're editing on GitHub" '((cl-lib "0.5")) :commit "9742a5183af853788c6ecb83fb7ee0b00d1675ac" :url "https://github.com/osener/github-browse-file" :keywords '("convenience" "vc" "git" "github"))
