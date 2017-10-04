576d142639c86d691dd3018ded6a786dcb4ae8fd
Ricky Pinder <pinderrr@outlook.com>
2017-10-04 22:40:30 +0100

    use-package automatically installed on startup

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -8,3 +8,16 @@
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
 (menu-bar-mode -1)
+
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(package-initialize)
+
+(unless (package-installled-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(require 'use-package)
