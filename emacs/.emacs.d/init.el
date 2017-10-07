b7af6737b048aa5ae3db7b9553b5740898b05a29
Ricky Pinder <pinderrr@outlook.com>
2017-10-07 23:25:59 +0100

    helm-gtags

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -133,4 +133,26 @@
   :ensure t
   :bind ("C-s". helm-swoop))
 
+(use-package helm-gtags
+  :ensure t
+  :bind
+  ("C-c g a" . helm-gtags-in-this-function)
+  ("C-j" . helm-gtags-select)
+  ("M-." . helm-gtags-dwim)
+  ("M-," . helm-gtags-pop-stack)
+  ("C-c <" . helm-gtags-previous-history)
+  ("C-c >" . helm-gtags-next-history)
+:config
+(setq
+ helm-gtags-ignore-case t
+ helm-gtags-auto-update t
+ helm-gtags-use-input-at-cursor t
+ helm-gtags-pulse-at-cursor t
+ helm-gtags-prefix-key "\C-cg"
+ helm-gtags-suggested-key-mapping t)
+(add-hook 'dired-mode-hook 'helm-gtags-mode)
+(add-hook 'eshell-mode-hook 'helm-gtags-mode)
+(add-hook 'c-mode-hook 'helm-gtags-mode)
+(add-hook 'c++-mode-hook 'helm-gtags-mode)
+(add-hook 'asm-mode-hook 'helm-gtags-mode))
 
