cb05338d7d3da40840166f66214390756cb2530d
Ricky Pinder <pinderrr@outlook.com>
2018-01-16 18:06:33 +0000

    added evil, evil-collection, evil-magit

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -75,6 +75,22 @@
 
 (require 'use-package)
 
+(use-package evil
+  :ensure t
+  :init
+  (setq evil-want-integration nil)
+  :config
+  (evil-mode 1))
+
+(use-package evil-collection
+  :after evil
+  :ensure t
+  :config
+  (evil-collection-init))
+
+(use-package evil-magit
+  :ensure t)
+
 (use-package hc-zenburn-theme
   :ensure t
   :config
